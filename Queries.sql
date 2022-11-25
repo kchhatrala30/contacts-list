8e4e98a915e6cb46b20cab2b8f882f97025a1adc
kchhatrala30 <118632329+kchhatrala30@users.noreply.github.com>
2022-11-24 21:27:45 -0500

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -5,8 +5,16 @@ USE CONTACTBOOK;
 # View Contacts Tester
 SELECT * FROM person;
 
+# Search for Contact Tester
+SELECT * FROM person where FirstName LIKE "%d@te%" OR LastName LIKE "%d@te%" OR PhoneNum LIKE "%d@te%" OR Email LIKE "%d@te%";
+
 # Add Contacts Tester
-INSERT INTO person VALUES("TestFirst", "TestLast", "1234567890", "tester@test.com");
+INSERT INTO person VALUES("TestFirst", "Test01", "1234567890", "first@test.com");
+INSERT INTO person VALUES("TestSecond", "Test02", "0987654321", "second@test.com");
+INSERT INTO person VALUES("TestThird", "Test03", "0123456789", "third@test.com");
 
 # Delete Contacts Tester
 DELETE FROM person WHERE FirstName = "TestFirst" AND LastName = "TestLast";
+
+# Clear Table for Testing
+DELETE FROM person;
