5fd788a243054c8f006beeddc1d7f03afb5fba4d
kchhatrala30 <118632329+kchhatrala30@users.noreply.github.com>
2022-12-20 16:12:34 -0500

    Add files via upload

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,6 @@
 USE CONTACTBOOK;
 
-# Table Data: FirstName char(20), LastName char(20), PhoneNum char(10), Email char(30)
+# Table Data: FirstName char(20), LastName char(20), PhoneNum char(15), Email char(30)
 
 # View Contacts Tester
 SELECT * FROM person;
@@ -14,7 +14,7 @@ INSERT INTO person VALUES("TestSecond", "Test02", "0987654321", "second@test.com
 INSERT INTO person VALUES("TestThird", "Test03", "0123456789", "third@test.com");
 
 # Delete Contacts Tester
-DELETE FROM person WHERE FirstName = "TestFirst" AND LastName = "TestLast";
+DELETE FROM person WHERE FirstName = "TestSecond" AND LastName = "Test02";
 
 # Clear Table for Testing
 DELETE FROM person;
